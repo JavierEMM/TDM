826dfb1326a147935f2b3f48c4f77248d7e84e96
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-11-17 16:44:47 -0500

    TI Activities

---
+++ app/src/main/java/pe/edu/pucp/tdm/ti/EditarDispositivoActivity.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.tdm.ti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.edu.pucp.tdm.R;
+
+public class EditarDispositivoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_dispositivo);
+    }
+}
