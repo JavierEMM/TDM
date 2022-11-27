4787d81ed33876798a504f5e7dad82138312b349
Diego Vargas <a20191027@pucp.edu.pe>
2022-11-27 12:28:42 -0500

    Avance intent xd

--- app/src/main/java/pe/edu/pucp/tdm/cliente/ClienteDetallesDispositivoActivity.java
+++ app/src/main/java/pe/edu/pucp/tdm/cliente/ClienteDetallesDispositivoActivity.java
@@ -2,9 +2,14 @@ package pe.edu.pucp.tdm.cliente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.xml.sax.DTDHandler;
 
 import pe.edu.pucp.tdm.R;
+import pe.edu.pucp.tdm.dto.DispositivoDTO;
 
 public class ClienteDetallesDispositivoActivity extends AppCompatActivity {
 
@@ -12,6 +17,10 @@ public class ClienteDetallesDispositivoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_detalles_dispositivo);
-
+        Intent intent = getIntent();
+        DispositivoDTO dispositivo =(DispositivoDTO) intent.getSerializableExtra("dispositivo");
+        Log.d("msg","Se recepcionó"+dispositivo.getNombre());
     }
+
+
 }
